1c2375082a24c655882a57563dc96f282dbf7cbb
Deomid-King <deomid555@gmail.com>
2025-12-05 11:27:05 +0500

    Update weight values in calculate function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function calculate() {
 	var procent = document.getElementById("procent").value;
 
 	// рассчитываем результат
-	var output = (+price + +result +(weight == "5t" ? 150: weight == "15t" ? 1195 : weight == "5kg" ? 2255 : weight == "15kg" ? 2703 : weight == "50kg" ? 4831 : 8079)) / 0.83;
+	var output = (+price + +result +(weight == "5t" ? 596: weight == "15t" ? 1731 : weight == "5kg" ? 2571 : weight == "15kg" ? 2983 : weight == "50kg" ? 5111 : 8359)) / 0.83;
 
 	// расчет процентов
 	var procent = (+result / +price) * 100;
@@ -15,4 +15,5 @@ function calculate() {
 
 	// выводим результат на страницу
 	document.getElementById("output").innerHTML = "Итоговая цена для Каспи: " + Math.round(output);
+
 }
