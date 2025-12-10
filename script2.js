2f3e95c0cb9a1481e40c660c126363f6448bdb26
Deomid-King <deomid555@gmail.com>
2025-12-10 10:31:52 +0500

    Update calculation logic in calculate function

--- script2.js
+++ script2.js
@@ -5,7 +5,7 @@ function calculate() {
 	var procent = document.getElementById("procent").value;
 	
 	// рассчитываем результат
-	var output = +price * 0.83 - +result - (weight == "5t" ? 846: weight == "15t" ? 1981 : weight == "5kg" ? 2921 : weight == "15kg" ? 3233 : weight == "50kg" ? 5361 : 8609);
+	var output = +price * 0.83 - +result - (weight == "5t" ? 596: weight == "15t" ? 1731 : weight == "5kg" ? 2571 : weight == "15kg" ? 2983 : weight == "50kg" ? 5111 : 8359);
 
 	// расчет процентов
 	var procent = (+output / +result) * 100;
@@ -17,4 +17,5 @@ function calculate() {
 	document.getElementById("output").innerHTML = "Маржа: " + Math.round(output);
 
 }
+
 
